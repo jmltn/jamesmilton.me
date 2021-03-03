4141d4bbfc02f0eada7e6f63c2a8f19924194321
SmithKy3 <1250smit@gmail.com>
2021-03-03 21:17:55 +0000

    Tidy up JS and fix page hang while gifs load in

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,273 +1,234 @@
-(function($) {
-	var $window = $(window),
-		$body = $('body'),
-		$featured = $('#featured-slider');
+'use strict';
+/*------------------------------------------------*/
+// GLOBAL FUNCTIONS / CONSTANTS
+/*------------------------------------------------*/
+function isElementInViewport(element) {
+	const bounds = element.getBoundingClientRect();
+	return bounds.top >= 0
+		&& bounds.left >= 0
+		&& bounds.bottom <= window.innerHeight
+		&& bounds.right <= window.innerWidth;
+}
 
-	$(document).ready(function() {
+function getRandomArrayEntry(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
 
+function getRandomCharacter() {
+	return String.fromCharCode(Math.random() * (127 - 33) + 33);
+}
 
-		// Timeline show More
-		var list = $(".listTimeline li");
-	  var numToShow = 3;
-	  var button = $("#next");
-		var fade = $(".fade");
-		var numInList = list.length;
-	  list.hide();
-	  if (numInList > numToShow) {
-	    button.show();
-	  }
-	  list.slice(0, numToShow).show();
+const gridLayout = (function() {
+	const $container = $('#post-wrapper');
+	const $gridItems = $container.children().addClass('post--loaded');
+	let initialized = false;
 
-	  button.click(function () {
-	    var showing = list.filter(":visible").length;
-	    list.slice(showing - 1, showing + numToShow).fadeIn();
-	    var nowShowing = list.filter(":visible").length;
-	    if (nowShowing >= numInList) {
-	      button.hide();
-				fade.hide();
+	function init() {
+		$container.masonry({
+			itemSelector: '.post',
+			columnWidth: '.post',
+			transitionDuration: 0
+		});
+		showItems($gridItems);
+		initialized = true;
+		$container.imagesLoaded(function() {
+			$container.masonry('layout');
+		});
+	}
+	function refresh() {
+		if (!initialized) {
+			init();
+			return;
+		}
+		$container.masonry('layout');
+	}
+	function showItems($gridItems) {
+		$gridItems.each(function(index, item) {
+			const $postInside = $(item).find('.post-inside');
+			animatePost($postInside, index * 100);
+		});
+	}
+	function animatePost($postInside, delay) {
+		setTimeout(function() {
+			$postInside.addClass('is--visible');
+		}, delay);
+	};
+	return { init, refresh };
+})();
+
+function typeSkills() {
+	function getRandomColoredString(numberOfCharacters) {
+	    const fragment = document.createDocumentFragment();
+	    for (var i = 0; i < numberOfCharacters; i++) {
+	        const span = document.createElement('span');
+	        span.textContent = getRandomCharacter();
+	        span.style.color = getRandomArrayEntry(COLORS);
+	        fragment.appendChild(span);
 	    }
-	  });
-
-
-		// Hidden sections
-		$('#show-sidebar, #hide-sidebar').on('click', function (e) {
-			$body.toggleClass('sidebar--opened');
-			e.preventDefault();
-		});
-		$('#site-overlay').on('click', function(e){
-			$body.removeClass('sidebar--opened search--opened');
-			searchField.clear();
-			e.preventDefault();
-		});
-
-		// Featured carousel
-		$featured.slick({
-			autoplay: true,
-			arrows : true,
-			dots : false,
-			fade : true,
-			appendArrows : $('.featured-nav'),
-			prevArrow : $('.featured-prev'),
-			nextArrow : $('.featured-next')
-		});
-		$featured.fadeIn(600, function(){
-			$featured.parents().removeClass('slider-loading');
-		});
-
-		// Back to top button
-		$('#top-link').on('click', function(e) {
-			$('html, body').animate({'scrollTop': 0});
-			e.preventDefault();
-		});
-		$window.scroll(function () {
-			if ( $(this).scrollTop() > 600 ) {
-				$body.addClass('is--scrolled');
-			} else {
-				$body.removeClass('is--scrolled');
-			}
-		});
-
-		// Responsive videos
-		$('.post').fitVids();
-
-		// Image adjustments
-		if ( $body.hasClass( 'post-template' ) || $body.hasClass( 'page-template' ) ) {
-			adjustImages();
-		}
-
-		// Grid layout
-		if ( $.isFunction( $.fn.masonry ) && $('#post-wrapper').length ) {
-			gridLayout.refresh();
-		}
-
-	});
-
-	$window.on('debouncedresize', onResize);
-
-	var gridLayout = (function() {
-		var $container = $('#post-wrapper'),
-			$items = $container.children().addClass('post--loaded'),
-			initialized = false,
-			init = function() {
-				$container.imagesLoaded(function() {
-					$container.masonry({
-						itemSelector: '.post',
-						columnWidth: '.post',
-						transitionDuration: 0
-					});
-					setTimeout(function() {
-						$container.masonry('layout');
-					}, 100);
-					showItems($items);
-					initialized = true;
-				});
-			},
-			refresh = function() {
-				if (!initialized) {
-					init();
-					return;
-				}
-				$container.masonry('layout');
-			},
-			showItems = function($items) {
-				$items.each(function(i, obj) {
-					var $postInside = $(obj).find('.post-inside');
-					animatePost($postInside, i * 100);
-				});
-			},
-			animatePost = function($postInside, delay) {
-				setTimeout(function() {
-					$postInside.addClass('is--visible');
-				}, delay);
-			};
-		return {
-			init: init,
-			refresh: refresh
-		}
-	})();
-
-	function onResize() {
-		if ( $body.hasClass( 'post-template' ) || $body.hasClass( 'page-template' ) ) {
-			adjustImages();
-		}
-		if ( $.isFunction( $.fn.masonry ) && $('#post-wrapper').length ) {
-			gridLayout.refresh();
-		}
+	    return fragment;
 	}
 
-
-	var prefix = '';
-	var skills = [
+	const SKILLS = [
 	    'Digital designer',
 	    '2D/3D creator',
-			'Motion designer',
-			'VR/AR specialist',
-			'Video maker',
-			'Snowboard sender',
-			'Guitarist'
-
-	].map(function (s) { return s; });
-	var delay = 10;
-	var step = 1;
-	var tail = 5;
-	var timeout = 75;
-	var p1 = document.getElementById('para1');
-	var colors = [
+		'Motion designer',
+		'VR/AR specialist',
+		'Video maker',
+		'Snowboard sender',
+		'Guitarist'
+	];
+	const COLORS = [
 	    "#f26a79",
 	    "#18a5b7",
 	    "#f7e88a"
 	];
-	function getRandomColor() {
-	    return colors[Math.floor(Math.random() * colors.length)];
-	}
-	function getRandomChar() {
-	    return String.fromCharCode(Math.random() * (127 - 33) + 33);
-	}
-	function getRandomColoredString(n) {
-	    var fragment = document.createDocumentFragment();
-	    for (var i = 0; i < n; i++) {
-	        var char = document.createElement('span');
-	        char.textContent = getRandomChar();
-	        char.style.color = getRandomColor();
-	        fragment.appendChild(char);
-	    }
-	    return fragment;
-	}
-	/** Global State */
-	var kye = {
+	const DEFAULT_DELAY = 10;
+	const MAX_TRAILING_CHARACTERS = 5;
+	const DEFAULT_TIMEOUT = 90;
+	const SKILL_STATE = {
 	    text: ' ',
-	    prefixP: -tail,
-	    skillI: 0,
+	    prefixP: -MAX_TRAILING_CHARACTERS,
+	    currentSkillIndex: 0,
 	    skillP: 0,
 	    direction: 'forward',
-	    delay: delay,
-	    step: step,
+	    delay: DEFAULT_DELAY,
+	    currentCharacterIndex: 1,
 	};
-	function render() {
-	    var skill = skills[kye.skillI];
-	    if (kye.step) {
-	        kye.step--;
-	    }
-	    else {
-	        kye.step = step;
-	        if (kye.prefixP < prefix.length) {
-	            if (kye.prefixP >= 0) {
-	                kye.text += prefix[kye.prefixP];
-	            }
-	            kye.prefixP++;
-	        }
-	        else {
-	            if (kye.direction === 'forward') {
-	                if (kye.skillP < skill.length) {
-	                    kye.text += skill[kye.skillP];
-	                    kye.skillP++;
-	                }
-	                else {
-	                    if (kye.delay) {
-	                        kye.delay--;
-	                    }
-	                    else {
-	                        kye.direction = 'backward';
-	                        kye.delay = delay;
-	                    }
-	                }
-	            }
-	            else {
-	                if (kye.skillP > 0) {
-	                    kye.text = kye.text.slice(0, -1);
-	                    kye.skillP--;
-	                }
-	                else {
-	                    kye.skillI = (kye.skillI + 1) % skills.length;
-	                    kye.direction = 'forward';
-	                }
-	            }
-	        }
-	    }
-	    p1.textContent = kye.text;
-	    p1.appendChild(getRandomColoredString(kye.prefixP < prefix.length ?
-	        Math.min(tail, tail + kye.prefixP) :
-	        Math.min(tail, skill.length - kye.skillP)));
-	    setTimeout(render, timeout);
+	const skillTextContainer = document.getElementById('para1');
 
+	function drawNextCharacter() {
+	    const skill = SKILLS[SKILL_STATE.currentSkillIndex];
 
+	    if (SKILL_STATE.currentCharacterIndex > 0) SKILL_STATE.currentCharacterIndex--;
+
+		if (SKILL_STATE.currentCharacterIndex === 0) {
+			SKILL_STATE.currentCharacterIndex = 1;
+
+	        if (SKILL_STATE.direction === 'forward') {
+				if (SKILL_STATE.skillP < skill.length) {
+					SKILL_STATE.text += skill[SKILL_STATE.skillP];
+					SKILL_STATE.skillP++;
+				}
+				else {
+					if (SKILL_STATE.delay > 0) SKILL_STATE.delay--;
+					else {
+						SKILL_STATE.direction = 'backward';
+						SKILL_STATE.delay = DEFAULT_DELAY;
+					}
+				}
+			} else if (SKILL_STATE.skillP > 0) {
+				SKILL_STATE.text = SKILL_STATE.text.slice(0, -1);
+				SKILL_STATE.skillP--;
+			} else {
+				SKILL_STATE.currentSkillIndex = SKILL_STATE.currentSkillIndex == SKILLS.length - 1
+					? 0
+					: SKILL_STATE.currentSkillIndex + 1;
+				SKILL_STATE.direction = 'forward';
+			}
+	    }
+
+	    skillTextContainer.textContent = SKILL_STATE.text;
+		const numberOfTrailingChars = Math.min(MAX_TRAILING_CHARACTERS, skill.length - SKILL_STATE.skillP);
+	    skillTextContainer.appendChild(getRandomColoredString(numberOfTrailingChars));
+	    setTimeout(drawNextCharacter, DEFAULT_TIMEOUT);
 	}
-	setTimeout(render, 500);
+	drawNextCharacter();
+}
 
+/*------------------------------------------------*/
+// THINGS THAT NEED TO HAPPEN ON PAGE-LOAD
+/*------------------------------------------------*/
+window.addEventListener('DOMContentLoaded', function() {
+	const $window = $(window);
 
-})(jQuery);
+	// Start header skills typing
+	typeSkills();
 
-// Timeline
-(function() {
+	// Timeline show more button click event
+	const list = $(".listTimeline li");
+	const numToShow = 3;
+	const button = $("#next");
+	const fade = $(".fade");
+	const numInList = list.length;
+	list.hide();
+	if (numInList > numToShow) {
+	  button.show();
+	}
+	list.slice(0, numToShow).show();
 
-  'use strict';
+	button.on('click', () => {
+	  const showing = list.filter(":visible").length;
+	  list.slice(showing - 1, showing + numToShow).fadeIn();
+	  const nowShowing = list.filter(":visible").length;
+	  if (nowShowing >= numInList) {
+		button.hide();
+		fade.hide();
+	  }
+	});
 
-  // define variables
-  var items = document.querySelectorAll(".timeline li");
+	// Hidden sections
+	$('#show-sidebar, #hide-sidebar').on('click', event => {
+		document.body.classList.toggle('sidebar--opened');
+		event.preventDefault();
+	});
+	$('#site-overlay').on('click', event => {
+		document.body.classList.remove('sidebar--opened');
+		document.body.classList.remove('search--opened');
+		event.preventDefault();
+	});
 
-  // check if an element is in viewport
-  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-  function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+	// Initialise featured work carousel and the fade it in
+	$('#featured-slider').slick({
+		autoplay: true,
+		arrows : true,
+		dots : false,
+		fade : true,
+		appendArrows : $('.featured-nav'),
+		prevArrow : $('.featured-prev'),
+		nextArrow : $('.featured-next')
+	}).fadeIn(600, function() {
+		$(this).parents().removeClass('slider-loading');
+	});
 
-  function callbackFunc() {
-    for (var i = 0; i < items.length; i++) {
-      if (isElementInViewport(items[i])) {
-        items[i].classList.add("in-view");
-      }
-    }
-  }
+	// Back to top button
+	$('#top-link').on('click', event => {
+		$('html, body').animate({'scrollTop': 0});
+		event.preventDefault();
+	});
+	$window.on('scroll', () => {
+		if ($window.scrollTop() > 600) {
+			document.body.classList.add('is--scrolled');
+		} else {
+			document.body.classList.remove('is--scrolled');
+		}
+	});
 
-  // listen for events
-  window.addEventListener("load", callbackFunc);
-  window.addEventListener("resize", callbackFunc);
-  window.addEventListener("scroll", callbackFunc);
+	// Responsive videos
+	$('.post').fitVids();
 
-})();
+	// Grid layout
+	if (typeof $.fn.masonry === 'function' && $('#post-wrapper').length ) {
+		gridLayout.refresh();
+		$window.on('debouncedresize', () => {
+			gridLayout.refresh();
+		});
+	}
+
+	// Timeline event bindings
+	const IN_VIEW_CLASSNAME = 'in-view';
+	const timelineElements = Array.from(document.querySelectorAll('.timeline li'));
+	function updateTimelineClasses() {
+		timelineElements.forEach(el => {
+			if (isElementInViewport(el)) {
+				el.classList.add(IN_VIEW_CLASSNAME);
+			}
+
+			if (timelineElements.every(el => el.classList.contains(IN_VIEW_CLASSNAME))) {
+				window.removeEventListener('resize', updateTimelineClasses);
+				window.removeEventListener('scroll', updateTimelineClasses);
+			};
+		});
+	}
+	window.addEventListener('resize', updateTimelineClasses);
+	window.addEventListener('scroll', updateTimelineClasses);
+});
